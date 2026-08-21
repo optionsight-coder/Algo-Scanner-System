cdc043aaf3e72c83a07120ffa831ce3a898be65b
optionsight-coder <option.sight@gmail.com>
2026-08-22 04:23:52 +0530

    Update SECRET_KEY in data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -5,7 +5,7 @@ import os
 from fyers_apiv3 import fyersModel
 
 APP_ID = "ZF5ZUHTUQN-100"
-SECRET_KEY = "MYQSBOIJDA"
+SECRET_KEY = "JG9KVFX9ST"
 REDIRECT_URI = "https://127.0.0.1"
 TOKEN_FILE = "fyers_token.txt"
 
